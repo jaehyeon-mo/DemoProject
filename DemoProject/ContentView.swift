b9c4d1857f61ec2f30dd0db1ed751092f42bb21f
jaehyeon <mojaehyeon@naver.com>
2025-09-10 15:55:36 +0900

    ObservableObject와 @StateObject를 활용한 상태 관리 예제 구현

--- DemoProject/ContentView.swift
+++ DemoProject/ContentView.swift
@@ -9,25 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var wifiEnabled: Bool = true
+//    @State private var wifiEnabled: Bool = true
+    
+    @StateObject var demoData: DemoData = DemoData()
     
     var body: some View {
-        Toggle(isOn: $wifiEnabled) {
-            Text("Enabled Wi-Fi")
-        }
-        WifiImageView(wifiEnabled: $wifiEnabled)
+//        Toggle(isOn: $wifiEnabled) {
+//            Text("Enabled Wi-Fi")
+//        }
+//        WifiImageView(wifiEnabled: $wifiEnabled)
+        
+        Text("\(demoData.currentUser), you are user number \(demoData.userCount)")
     }
 }
 
-struct WifiImageView: View {
-    
-    @Binding var wifiEnabled: Bool
-    
-    var body: some View {
-        Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
-    }
-}
+//struct WifiImageView: View {
+//    
+//    @Binding var wifiEnabled: Bool
+//    
+//    var body: some View {
+//        Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
+//    }
+//}
 
 #Preview {
-    ContentView()
+    ContentView(demoData: DemoData())
 }
