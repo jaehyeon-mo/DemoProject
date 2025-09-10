0de05727c7e127f99cb67ed0703c8ae906f6f497
jaehyeon <mojaehyeon@naver.com>
2025-09-10 12:59:03 +0900

    Initial Commit

---
+++ DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by 모재현 on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoProjectTests/DemoProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  DemoProjectTests.swift
+//  DemoProjectTests
+//
+//  Created by 모재현 on 9/10/25.
+//
+
+import Testing
+
+struct DemoProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
